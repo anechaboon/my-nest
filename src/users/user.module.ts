82fafb27e774cf47289f8376bd2ef4f0c984ca0d
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2026-01-07 11:49:06 +0700

    fix call api to users as 404

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { PrismaUserRepository } from './prisma-user.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { USER_REPOSITORY } from './user.repository.token';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [UsersController],
   providers: [
     UsersService,
     {
